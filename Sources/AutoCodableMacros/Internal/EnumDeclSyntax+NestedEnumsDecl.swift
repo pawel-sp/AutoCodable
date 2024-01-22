773c0236e044f77bdc0a2f4a0e11d6f1a7c760d6
Paweł Sporysz <pawel.sporysz@gmail.com>
2024-01-22 23:15:50 +0100

    Fix detecting nested coding keys

--- Sources/AutoCodableMacros/Internal/EnumDeclSyntax+NestedEnumsDecl.swift
+++ Sources/AutoCodableMacros/Internal/EnumDeclSyntax+NestedEnumsDecl.swift
@@ -2,7 +2,7 @@ import SwiftSyntax
 
 extension EnumDeclSyntax {
     func nestedEnumsDecl(named names: [String]) -> NamedDeclArray<EnumDeclSyntax> {
-        names.map { ($0, "\($0.capitalized)CodingKeys") }.compactMap {
+        names.map { ($0, "\($0.prefix(1).uppercased() + $0.dropFirst())CodingKeys") }.compactMap {
             if let enumDecl = enumDecl(named: $0.1, ofType: "CodingKey") {
                 ($0.0, enumDecl)
             } else {
